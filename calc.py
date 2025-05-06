797adf53ab46e55f369d8f9eb8715871826ed539
Gabriel Valente <gabrielmv1121@gmail.com>
2025-05-06 16:46:43 -0400

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,9 @@
+numeros1 = (1, 2, 3)
+numeros2 = (4, 5 ,6 )
+
+todos_os_numeros = numeros1 + numeros2
+
+print(f"Resultado das tuplas concatenadas: {todos_os_numeros}")
+
+contar = numeros1.count(2)
+print(f"O número dois é repetido: {contar} vezes")
